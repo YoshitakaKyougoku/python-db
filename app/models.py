ce752682059f5aed948d0a2d75533daa34b556a6
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-10-24 11:13:11 +0900

    modelファイル

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer,\
+    String, Text, text, create_engine,\
+        DATETIME, Boolean, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+import user.config as conf
+
+URL = '{}://{}:{}@{}:{}/{}'.format(conf.DATABASE,\
+    conf.DB_USER, conf.DB_PASS, conf.DB_HOST, conf.DB_PORT, conf.DB_NAME)
+engin = create_engine(URL, echo=True)
+
+Base = declarative_base()
+Connection = sessionmaker(bind=engin)
+connection = Connection()
+
+class Books(Base):
+    __tablename__ = "books"
+    id = Column('id', Integer, primary_key = True)
+    name = Column('name', String(255))
+    volume = Column('volume', String(255))
+    author = Column('author', String(255))
+    publisher = Column('publisher', String(255))
+    memo = Column('memo', Text())
+    create_date = Column('create_date',\
+        DateTime(timezone=True), default=func.now(), nullable=False)
+    delFlg = Column('del', Boolean)
+    
+class BookUser(Base):
+    __tablename__ = "book_user"
+    user_id = Column('user_id', Integer, primary_key = True)
+    passwd = Column('passwd', String(255), nullable=False)
+    email = Column('email', String(255), nullable=False)
+    user_shi = Column('user_shi', String(255))
+    user_mei = Column('user_mei', String(255))
+    delFlg = Column('del', Boolean)
+    
+if __name__ == "__main__":
+    Base.metadata.create_all(engin)
